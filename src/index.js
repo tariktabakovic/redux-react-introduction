2fe9ecd263439b0e65aa8599e95768d8ffb62c1c
tariktabakovic <tariktab97@gmail.com>
2020-02-10 10:47:28 -0500

    creates counter function and imports createStore from redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// The Bank - state
+// Describe the ideal version of state. 
+// {
+//     amount: 100
+// }
+// if we added 1 to the amount, what would state look like
+// {
+//     amount: 101
+// }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// A Transaction Slip - action 
+// {
+//     type: 'INCREMENT'
+// }
+
+// {
+//     type: 'DECREMENT'
+// }
+
+import {
+    createStore
+} from 'redux';
+// The Teller - reducer function 
+// reducers are always named for the state they manage. they always recieve 
+// the current state and the action they're processin. 
+function counter(state, action){
+    const newState= {...state};
+    if (action.type === 'INCREMENT'){
+        newState.amount = state.amount + 1;
+    } else if (action.type ==='DECREMENT'){
+        newState.amount = state.amount - 1;
+    } else {
+        // no need to do anything here. we already made a copy of state to return.
+    }
+    // They must return the new version of state  
+    return newState;
+}
+
+// You give it  a reducer, it gives you a 'store'.
+// The store is an object that manages your state using you reducer. 
+const store = createStore(counter);
+
+
+// Push Notifications - subscribe to changes in the store
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
